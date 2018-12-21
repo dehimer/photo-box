2b69a6ad4853166fee7a53287d3b987268a0660b
Denis Novikov <den-vse@yandex.ru>
2018-12-21 22:43:17 +0300

    feature: update view of buttons

--- src/client/components/Gallery/components/Footer.js
+++ src/client/components/Gallery/components/Footer.js
@@ -1,16 +1,29 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { styled } from '@material-ui/styles';
 import Button from '@material-ui/core/Button';
 
-const Footer = ({ mode }) => {
+const PrintButton = styled(Button)({
+  background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+  border: 0,
+  borderRadius: 3,
+  boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+  color: 'white',
+  height: 100,
+  width: 300,
+  fontSize: 40,
+  margin: '0 20px'
+});
+
+const Footer = ({ mode, active }) => {
   let content = null;
 
   if (mode !== '') {
     content = (mode === 'print')
       ? (
         <div>
-          <Button variant="contained" color="primary">Print</Button>
-          <Button variant="contained" color="primary">Mail</Button>
+          <PrintButton variant="contained" color="primary" disabled={!active}>Print</PrintButton>
+          <PrintButton variant="contained" color="primary" disabled={!active}>Mail</PrintButton>
         </div>
       )
       : (<div className="intro">请选择下载的照片</div>);
@@ -28,6 +41,7 @@ Footer.defaultProps = {
 };
 
 Footer.propTypes = {
+  active: PropTypes.bool.isRequried,
   // eslint-disable-next-line react/forbid-prop-types
   mode: PropTypes.string
 };
