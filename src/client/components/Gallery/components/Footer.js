5cc5f560236838575af7d34403d0bce190af7939
Denis Novikov <den-vse@yandex.ru>
2018-12-21 21:12:41 +0300

    feature: install material-ui

--- src/client/components/Gallery/components/Footer.js
+++ src/client/components/Gallery/components/Footer.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Button from '@material-ui/core/Button';
 
 const Footer = ({ mode }) => {
   let content = null;
 
-  if (typeof mode !== '') {
+  if (mode !== '') {
     content = (mode === 'print')
       ? (
         <div>
-          <button type="button">Print</button>
-          <button type="button">Mail</button>
+          <Button variant="contained" color="primary">Print</Button>
+          <Button variant="contained" color="primary">Mail</Button>
         </div>
       )
       : (<div className="intro">请选择下载的照片</div>);
--- src/client/index.js
+++ src/client/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { HashRouter} from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './App';
 import configureStore from './store/configureStore';
+import 'typeface-roboto';
 
 const store = configureStore();
 
